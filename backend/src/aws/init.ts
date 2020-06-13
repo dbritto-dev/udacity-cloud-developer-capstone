5b0f8196c97bfc1dce24e821e4353300491c5d9f
danilobrinu <ddbn.c2@gmail.com>
2020-06-13 11:24:50 -0500

    Add aws instances

--- backend/src/aws/init.ts
+++
@@ -1,8 +0,0 @@
-import * as AWS from "aws-sdk";
-
-AWS.config.credentials = new AWS.Credentials({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
-
-export const dynamo = new AWS.DynamoDB();
---
+++ backend/src/aws/s3.ts
@@ -0,0 +1,3 @@
+import { AWS } from "./aws";
+
+export const s3 = new AWS.S3();
