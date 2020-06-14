ce09e59e1ca87505af5b384669aa5223c863924e
danilobrinu <ddbn.c2@gmail.com>
2020-06-14 16:03:07 -0500

    Add handler for update todo

--- backend/src/lambda/http/update-todo.ts
+++ backend/src/lambda/http/update-todo.ts
@@ -1,13 +1,38 @@
-import 'source-map-support/register'
+import "source-map-support/register";
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { dynamodb } from "../../aws";
+import { UpdateTodoRequest } from "../../requests/UpdateTodoRequest";
+import { createTodoItem } from "../../utils/todo";
+import { getUserId } from "../../auth/utils";
 
-import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  // DONE: Update a TODO item with the provided id using values in the "updatedTodo" object
+  const TABLE_NAME = process.env.TODOS_TABLE_NAME;
+  const userId = getUserId(event);
+  const { todoId = "" } = event.pathParameters;
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  try {
+    const request: UpdateTodoRequest = JSON.parse(event.body);
+    const todoItem = createTodoItem({ userId, request });
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
-}
+    await dynamodb
+      .update({
+        TableName: TABLE_NAME,
+        Key: { userId, todoId },
+        UpdateExpression: "SET #name = :name, dueDate = :dueDate, done = :done",
+        ExpressionAttributeNames: { "#name": todoItem.name },
+        ExpressionAttributeValues: Object.entries(request).reduce<Object>(
+          (output, [key, value]) => ({ ...output, [`:${key}`]: value }),
+          {}
+        ),
+      })
+      .promise();
+
+    return { statusCode: 204, body: null };
+  } catch (e) {
+    return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
+  }
+};
