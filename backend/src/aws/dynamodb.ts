41aaf75a1641d4398325aaa76bf53245b8eb8ff5
danilobrinu <ddbn.c2@gmail.com>
2020-06-14 01:47:04 -0500

    Update dynamodb service to support dynamodb locally

--- backend/src/aws/dynamodb.ts
+++ backend/src/aws/dynamodb.ts
@@ -1,3 +1,5 @@
 import { AWS } from "./aws";
 
-export const dynamodb = new AWS.DynamoDB();
+export const dynamodb = new AWS.DynamoDB.DocumentClient(
+  process.env.IS_OFFLINE ? { endpoint: "http://localhost:8000" } : {}
+);
