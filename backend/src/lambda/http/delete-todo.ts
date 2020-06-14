17d9e98ad509f6ad3abce205244788d3ee295d9b
danilobrinu <ddbn.c2@gmail.com>
2020-06-14 01:49:22 -0500

    Complete delete todo handler

--- backend/src/lambda/http/delete-todo.ts
+++ backend/src/lambda/http/delete-todo.ts
@@ -1,10 +1,28 @@
-import 'source-map-support/register'
+import "source-map-support/register";
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from "aws-lambda";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { dynamodb } from "../../aws";
+import { getUserId } from "../../auth/utils";
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  // DONE: Remove a TODO item by id
+  const userId = getUserId(event);
+  const { todoId = "" } = event.pathParameters;
 
-  // TODO: Remove a TODO item by id
-  return undefined
-}
+  console.log(userId, todoId);
+
+  try {
+    await dynamodb
+      .delete({
+        TableName: process.env.TODOS_TABLE_NAME,
+        Key: { userId, todoId },
+        ConditionExpression: "attribute_exists(todoId)",
+      })
+      .promise();
+    return { statusCode: 204, body: null };
+  } catch (e) {
+    return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
+  }
+};
