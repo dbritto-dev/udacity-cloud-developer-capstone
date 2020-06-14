e43865d9ccfed18d286240295464d02b719dab44
danilobrinu <ddbn.c2@gmail.com>
2020-06-14 14:35:07 -0500

    Update s3 local config

--- backend/src/aws/s3.ts
+++ backend/src/aws/s3.ts
@@ -1,3 +1,12 @@
-import { AWS } from "./aws";
+import { AWS, IS_OFFLINE } from "./aws";
 
-export const s3 = new AWS.S3();
+export const s3 = new AWS.S3(
+  IS_OFFLINE
+    ? {
+        s3ForcePathStyle: true,
+        endpoint: "http://localhost:12000",
+        accessKeyId: "S3RVER",
+        secretAccessKey: "S3RVER",
+      }
+    : {}
+);
