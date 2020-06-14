580a20a30b7824cc7acf1de9d5a20f835c1f1128
danilobrinu <ddbn.c2@gmail.com>
2020-06-14 01:48:41 -0500

    Complete get todos handler

--- backend/src/lambda/http/get-todos.ts
+++ backend/src/lambda/http/get-todos.ts
@@ -1,7 +1,27 @@
-import 'source-map-support/register'
+import "source-map-support/register";
+import { DynamoDB } from "aws-sdk";
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { dynamodb } from "../../aws";
+import { getUserId } from "../../auth/utils";
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-}
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  // DONE: Get all TODO items for a current user
+  const userId = getUserId(event);
+
+  try {
+    const response: DynamoDB.DocumentClient.QueryOutput = await dynamodb
+      .query({
+        TableName: process.env.TODOS_TABLE_NAME,
+        KeyConditionExpression: "userId = :userId",
+        ExpressionAttributeValues: { ":userId": userId },
+      })
+      .promise();
+
+    return { statusCode: 200, body: JSON.stringify({ items: response.Items }) };
+  } catch (e) {
+    return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
+  }
+};
