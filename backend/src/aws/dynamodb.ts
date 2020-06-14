e99cbf3c9eaa656364c8c5514f87deda8e5604a5
danilobrinu <ddbn.c2@gmail.com>
2020-06-14 14:34:53 -0500

    Update dynamodb local endpoint

--- backend/src/aws/dynamodb.ts
+++ backend/src/aws/dynamodb.ts
@@ -1,5 +1,5 @@
-import { AWS } from "./aws";
+import { AWS, IS_OFFLINE } from "./aws";
 
 export const dynamodb = new AWS.DynamoDB.DocumentClient(
-  process.env.IS_OFFLINE ? { endpoint: "http://localhost:8000" } : {}
+  IS_OFFLINE ? { endpoint: "http://localhost:10000" } : {}
 );
