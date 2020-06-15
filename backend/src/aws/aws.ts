19a49d53faf77f2881aa78e25708b09116f4289c
danilobrinu <ddbn.c2@gmail.com>
2020-06-14 20:52:48 -0500

    Clean files

--- backend/src/aws/aws.ts
+++
@@ -1,9 +0,0 @@
-import * as AWS from "aws-sdk";
-
-AWS.config.credentials = new AWS.SharedIniFileCredentials({
-  profile: process.env.AWS_PROFILE || "default",
-});
-
-export { AWS };
-
-export const IS_OFFLINE = process.env.IS_OFFLINE;
