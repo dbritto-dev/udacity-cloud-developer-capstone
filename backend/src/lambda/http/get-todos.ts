f05acfc45950f5f693c6b8fed75992d0bb73810e
danilobrinu <ddbn.c2@gmail.com>
2020-06-15 21:18:58 -0500

    Update get todos to support logging

--- backend/src/lambda/http/get-todos.ts
+++ backend/src/lambda/http/get-todos.ts
@@ -1,32 +1,31 @@
 import "source-map-support/register";
 import { DynamoDB } from "aws-sdk";
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import * as middy from "middy";
-import { cors } from "middy/middlewares";
+import middy from "@middy/core";
+import cors from "@middy/http-cors";
 
-import { dynamodb } from "../../aws";
-import { getUserId } from "../../auth/utils";
+import { getTodos } from "../data/todo";
+import { createLogger } from "../../utils/logger";
 
-const getTodo: APIGatewayProxyHandler = async (
+const logger = createLogger("todo");
+
+const getTodosHandler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   // DONE: Get all TODO items for a current user
-  const TABLE_NAME = process.env.TODOS_TABLE_NAME;
-  const userId = getUserId(event);
+  logger.info("Fetching todos");
 
   try {
-    const response: DynamoDB.DocumentClient.QueryOutput = await dynamodb
-      .query({
-        TableName: TABLE_NAME,
-        KeyConditionExpression: "userId = :userId",
-        ExpressionAttributeValues: { ":userId": userId },
-      })
-      .promise();
+    const response: DynamoDB.DocumentClient.QueryOutput = await getTodos(event);
+
+    logger.info("Todos was fetched successfully");
 
     return { statusCode: 200, body: JSON.stringify({ items: response.Items }) };
   } catch (e) {
+    logger.error("Todos was not fetched successfully", { error: e.message });
+
     return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
   }
 };
 
-export const handler: APIGatewayProxyHandler = middy(getTodo).use(cors());
+export const handler: APIGatewayProxyHandler = middy(getTodosHandler).use(cors());
