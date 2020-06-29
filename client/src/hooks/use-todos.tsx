38ffe1ae82185f796a5cb3a500e44f1d95dc2b64
danilobrinu <ddbn.c2@gmail.com>
2020-06-29 16:01:10 -0500

    Update use todos

---
+++ client/src/hooks/use-todos.tsx
@@ -0,0 +1,20 @@
+import { useQuery, QueryResult } from 'react-query';
+import { useToken } from 'hooks/use-token';
+import { createFetchWithToken } from 'utils/unfetch';
+
+type Todos = {
+  items: Array<any>;
+};
+
+export const useTodos = (): QueryResult<Todos> => {
+  const token = useToken();
+  const response = useQuery(
+    [`todos:all`, token],
+    (token) => createFetchWithToken(token)<Todos>(`${process.env.REACT_APP_API_URI}/todos`),
+    {
+      enabled: Boolean(token),
+    }
+  );
+
+  return response;
+};
