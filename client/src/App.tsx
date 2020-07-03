c0ceca979a045800dbccc4e07c415e99ba17a0b4
danilobrinu <ddbn.c2@gmail.com>
2020-07-02 19:31:14 -0500

    Update App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import './styles.css';
 
 import { ProtectedRoute } from 'components/ProtectedRoute';
 import { DashboardScreen } from 'screens/DashboardScreen';
+import { EditTodosItemScreen } from 'screens/EditTodosItemScreen';
 import { RedirectingScreen } from 'screens/RedirectingScreen';
 
 const history = createBrowserHistory();
@@ -13,6 +14,11 @@ function App() {
   return (
     <Router history={history}>
       <Switch>
+        <ProtectedRoute
+          path="/:todoId"
+          component={EditTodosItemScreen}
+          onRedirecting={() => <RedirectingScreen />}
+        />
         <ProtectedRoute
           path="/"
           component={DashboardScreen}
